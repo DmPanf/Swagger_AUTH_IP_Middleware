3cb5871ccc7d7b28b0e51d0c7d009529495e6b20
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-11-08 21:24:35 +0300

    Create items1.py

---
+++ Swagger/items1.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/predict/{item_id}")
+async def read_item(item_id: int):
+    # Здесь будет логика вашего предсказания
+    return {"item_id": item_id}
