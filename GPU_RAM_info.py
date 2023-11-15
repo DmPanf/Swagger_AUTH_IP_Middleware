b03c5d641f40b5a65ef2793e10786bbf786b3c32
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-11-15 03:30:23 +0300

    Create GPU_RAM_info.py

---
+++ GPU_RAM_info.py
@@ -0,0 +1,26 @@
+# pip install fastapi uvicorn gputil psutil
+
+from fastapi import FastAPI, Request
+import GPUtil
+import psutil
+
+app = FastAPI()
+
+@app.get("/info")
+async def read_root(request: Request):
+    client_ip = request.client.host
+
+    # Получение информации о GPU
+    gpus = GPUtil.getGPUs()
+    gpu_info = [{'name': gpu.name, 'load': gpu.load, 'free_memory': gpu.memoryFree, 'total_memory': gpu.memoryTotal, 'temperature': gpu.temperature} for gpu in gpus]
+
+    # Получение информации о RAM
+    ram = psutil.virtual_memory()
+    ram_info = {'total': ram.total, 'available': ram.available, 'used': ram.used, 'percent': ram.percent}
+
+    return {
+        'Project 2023': 'Pothole Detection [Moscow, 2023 г.]',
+        'Client IP': client_ip,
+        'GPU Info': gpu_info,
+        'RAM Info': ram_info
+    }
